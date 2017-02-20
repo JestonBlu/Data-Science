9b1d86e5a1a101a94511d192b32a21595c065e22
jestonblu <jestonblu@gmail.com>
2017-02-20 21:30:26 +0000

    started building out gitbook

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output = 'bookdown::gitbook', output_dir = 'docs')"
